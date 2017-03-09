7859a518e4bfe06628e07e4ec217deec9eef6763
jordan morra-taroudjit <m-t.jordan@live.fr>
2017-03-09 09:23:10 +0100

    clean repo + barrel shift_count size changed

--- vhd/Barrel_shifter.vhd
+++ vhd/Barrel_shifter.vhd
@@ -9,7 +9,7 @@ entity Barrel_shifter is
     N : positive);
   port (
     Shifter_in : in std_logic_vector(N-1 downto 0);
-    Shift_count : in std_logic_vector(N-1 downto 0);                         
+    Shift_count : in std_logic_vector(3 downto 0);                         
     Shifter_out : out std_logic_vector(N-1 downto 0));
 end entity Barrel_shifter;
 
