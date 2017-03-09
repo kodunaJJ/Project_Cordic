8c23a1d3afbf817d3a429294a233188a86220ed2
jordan morra-taroudjit <m-t.jordan@live.fr>
2017-03-09 08:15:02 +0100

    ajout barrel

--- vhd/barrel_shifter.vhd
+++ vhd/barrel_shifter.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.math_real.all;
+use ieee.numeric_std.all;
 
 
 entity barrel_shifter is
@@ -8,16 +8,26 @@ entity barrel_shifter is
   generic (
     N : positive);
   port (
-    Barrel_in : in std_logic_vector(N-1 downto 0);
-    Shift_cmd : in std_logic_vector(LOG(2,N) downto 0); -- suppose que N est
-                                                        -- multiple de 2
-    Barrel_out : out std_logic_vector(N-1 downto 0));
+    Shifter_in : in std_logic_vector(N-1 downto 0);
+    Shift_count : in std_logic_vector(N-1 downto 0);                         
+    Shifter_out : out std_logic_vector(N-1 downto 0));
 end entity barrel_shifter;
 
 architecture A of barrel_shifter is
 
+  --signal bit_fill : std_logic_vector(to_integer(unsigned(Shift_count))-1 downto 0) ;
+  
 begin  -- architecture A
 
+  --if Shifter_in(N-1) = '1' then bit_fill <= (others => '1');
+  --else
+    --bit_fill <= (others => '0');
+  --end if;
   
+  --Shifter_out <= (bit_fill & ( Shifter_in(N-1 downto to_integer(unsigned(Shift_count))))); --
+  --ok ??
+  --Shifter_out <= Shifter_in sra 2;--to_integer(unsigned(Shift_count));
+  Shifter_out <= std_logic_vector(shift_right(signed(Shifter_in), to_integer(unsigned(Shift_count))));
+ 
 
 end architecture A;
