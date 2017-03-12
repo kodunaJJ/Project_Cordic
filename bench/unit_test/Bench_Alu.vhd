f1e3952aae5cfd81e28ddf0d6cd75a0fb4858a7a
taurus22294 <leanhdung.phelma@gmail.com>
2017-03-12 09:26:06 +0100

    testbench of Alu done

--- bench/unit_test/Bench_Alu.vhd
+++ bench/unit_test/Bench_Alu.vhd
@@ -13,7 +13,7 @@ architecture test of test_Alu is
   component Alu
 
     port (
-      A, B : in  std_logic_vector(15 downto 0);  --vrai ou pas
+      A, B : in  std_logic_vector(15 downto 0);
       Cmd  : in  std_logic;
       S    : out std_logic_vector(15 downto 0));
   end component Alu;
@@ -22,30 +22,62 @@ architecture test of test_Alu is
   signal Sig_A, Sig_B, Sig_S    : signed (15 downto 0);
   signal Sig_Cmd                : std_logic;
   signal Sig_As, Sig_Bs, Sig_Ss : std_logic_vector(15 downto 0);
-  signal Sig_Cmds               : std_logic;
-  constant delay_10             : time := 10 ns;
-  constant delay_5              : time := 5 ns;
-  constant delay_2              : time := 2 ns;
-  constant delay_1              : time := 1 ns;
+
+
+  constant delay_10 : time := 10 ns;
+  constant delay_5  : time := 5 ns;
+  constant delay_2  : time := 2 ns;
+  constant delay_1  : time := 1 ns;
 begin
   Alu1 : Alu
 
-  port map (
-    A   => Sig_As,
-    B   => Sig_Bs,
-    Cmd => Sig_Cmds,
-    S   => Sig_Ss);
-  Sig_As   <= std_logic_vector(Sig_A);
-  Sig_Bs   <= std_logic_vector(Sig_B);
-  Sig_Cmds <= Sig_Cmd;
-  Sig_S    <= signed(Sig_Ss);
+    port map (
+      A   => Sig_As,
+      B   => Sig_Bs,
+      Cmd => Sig_Cmd,
+      S   => Sig_Ss);
+  Sig_As <= std_logic_vector(Sig_A);
+  Sig_Bs <= std_logic_vector(Sig_B);
+  Sig_S  <= signed(Sig_Ss);
   process
   begin
     Sig_A   <= to_signed(10, 16);
     Sig_B   <= to_signed(15, 16);
-    Sig_Cmd <= '0';                              -- add
+    Sig_Cmd <= '0';                     -- add
+    wait for delay_5;
+    assert sig_S = 25 report "ALU test addition sig_out positive" severity note;
+
+    Sig_A   <= to_signed(10, 16);
+    Sig_B   <= to_signed(15, 16);
+    Sig_Cmd <= '1';                     -- minus
+    wait for delay_5;
+    assert sig_S = -5 report "ALU test soustraction sig_out negative" severity note;
+
+    Sig_A   <= to_signed(30, 16);
+    Sig_B   <= to_signed(12, 16);
+    Sig_Cmd <= '1';                     -- minus
+    wait for delay_5;
+    assert sig_S = 18 report "ALU test soustraction sig_out positive " severity note;
+
+    Sig_A   <= to_signed(10, 16);
+    Sig_B   <= to_signed(-15, 16);
+    Sig_Cmd <= '0';                     -- add
+    wait for delay_5;
+    assert sig_S = -5 report "ALU test addition sig_out negative" severity note;
+
+    Sig_A   <= to_signed(-12, 16);
+    Sig_B   <= to_signed(-18, 16);
+    Sig_Cmd <= '0';                     -- add
+    wait for delay_5;
+    assert sig_S = -30 report "ALU test addition sig_out negative" severity note;
+
+    Sig_A   <= to_signed(13, 16);
+    Sig_B   <= to_signed(-23, 16);
+    Sig_Cmd <= '1';                     -- minus
+    wait for delay_5;
+    assert sig_S = 10 report "ALU test soustraction sig_out positive" severity note;
 
     wait for delay_5;
-    assert sig_S = 25 report "ALU test sig_out add" severity note;
+    assert false report " FIN DE LA SIMULATION" severity failure;
   end process;
 end test;
