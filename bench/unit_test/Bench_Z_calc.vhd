159306799618b6c8b92deac8f2a3ca55016fb15a
jordan morra-taroudjit <m-t.jordan@live.fr>
2017-03-15 20:56:48 +0100

    change on signal generation

--- bench/unit_test/Bench_Z_calc.vhd
+++ bench/unit_test/Bench_Z_calc.vhd
@@ -40,21 +40,21 @@ architecture test of test_Z_calc is
   signal Sig_Angle       : std_logic_vector(15 downto 0);
   signal Sig_sel         : std_logic;
   signal Sig_Sign        : std_logic;
-  signal Sig_In_Enable   : std_logic := '0';
+  signal Sig_In_Enable   : std_logic                    := '0';
   signal Sig_Clk         : std_logic                    := '1';
   signal Sig_Reset       : std_logic;
   signal Sig_Msb         : std_logic;
   signal Sig_rom_address : std_logic_vector(3 downto 0);
   signal Sig_rom_out     : std_logic_vector(15 downto 0);
-  signal Sig_iteration   : std_logic_vector(3 downto 0) := "0001";
+  signal Sig_iteration   : std_logic_vector(3 downto 0) := "1111";
 
-  constant Clk_period       : time := 5 ns;
+  constant Clk_period       : time := 2 ns;
   constant iteration_period : time := 2*Clk_period;
-  constant simu_period      : time := 30*Clk_period;
+  constant simu_period      : time := 60*Clk_period;
 
 begin  -- architecture test
 
-  Sig_Sign <= not Sig_Msb;
+  
 
   Z_calc_test : Z_calc
     generic map (
@@ -76,16 +76,32 @@ begin  -- architecture test
       Rom_out     => Sig_Angle
       );
 
-  Sig_Clk         <= not Sig_Clk                                   after Clk_period;
-  Sig_iteration   <= std_logic_vector(unsigned(Sig_iteration) + 1) after 2*iteration_period;
-  Sig_In_Enable <= not Sig_In_Enable after 2*Clk_period;
+  Sig_Clk <= not Sig_Clk after Clk_period;
+  Sig_Sign <= not Sig_Msb;
+
+  Sig_iteration <= std_logic_vector(unsigned(Sig_iteration) + 1) after 2*iteration_period;
+
+  --Sig_In_Enable   <= not Sig_In_Enable after 2*Clk_period;
   Sig_rom_address <= Sig_iteration;
   Sig_Sign        <= not Sig_Msb;
+
+  process_count : process(Sig_Clk)
+  begin
+    if (Sig_Clk = '1' and Sig_Clk'event) then
+      if Sig_Reset = '1' then
+        Sig_In_Enable <= '0';
+      end if;
+      
+      Sig_In_Enable <= not Sig_In_Enable;
+
+    end if;
+  end process process_count;
 
   spy_process : process
   begin  -- process spy_process
     init_signal_spy("/test_Z_calc/Z_calc_test/Mux_out", "Mux_out", 1, -1);
     init_signal_spy("/test_Z_calc/Z_calc_test/Buff_OE", "buff_OE", 1, -1);
+    init_signal_spy("/test_Z_calc/Z_calc_test/result", "result", 1, -1);
     wait;
   end process spy_process;
 
@@ -98,26 +114,12 @@ begin  -- architecture test
     Sig_sel       <= '0';
     --Sig_In_Enable <= '0';
     --Sig_iteration <= "0001";
-    wait for 3*Clk_period;
+    wait for 5*Clk_period;
     assert false report "End reset period" severity note;
-
-    Sig_Reset     <= '0';
-    Sig_sel       <= '0';
-    --Sig_In_Enable <= '1';
-
-    wait for 4*Clk_period;
-    assert false report "Calculation start" severity note;
-
-    while unsigned(Sig_iteration) < 16 loop
-      Sig_sel       <= '1';
-      --Sig_In_Enable <= '1';
-
-      wait for Clk_period;
-
-      --Sig_In_Enable <= '0';
-      wait for Clk_period;
-
-    end loop;
+    Sig_Reset <= '0';
+    wait for 9*Clk_period;
+    Sig_sel <= '1';
+    
 
     wait for simu_period;
     assert false report "end OF SIMULATION" severity failure;
